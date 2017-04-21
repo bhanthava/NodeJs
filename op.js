e6aabaa7189d6618dcf6c3b74cac6ac3b4a1f137
bhanthava <bhanthava.k@gmail.com>
2017-04-20 19:49:32 -0500

    arthmetic 04/19

---
+++ op.js
@@ -0,0 +1,9 @@
+var sum = require('./arithmetic');
+var mul = require('./arithmetic');
+var sub = require('./arithmetic');
+var div = require('./arithmetic');
+
+console.log(sum.add(15,12));
+console.log(sum.mul(15,12));
+console.log(sum.sub(15,12));
+console.log(sum.div(15,12));
